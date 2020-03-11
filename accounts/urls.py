a7ed4da0bfcf82988692042f8e0de385681b2048
Anuphongkrystal <Anuphongkrystal@outlook.com>
2020-03-11 08:42:35 -0700

    login && register

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,6 +3,8 @@
 from django.urls import path
 from . import views #นำเข้าไฟล์ views.py
 urlpatterns = [
+    path('register/',views.registerPage,name="register"),
+    path('login/',views.loginPage,name="login"),
     path('',views.home,name="home"),
     path('products/',views.products,name="product"), #call function /views.py function products();
 
