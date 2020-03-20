b2aad02a0e225cd6df4688e6e4836081c0d7ebe9
Anuphongkrystal <Anuphongkrystal@outlook.com>
2020-03-20 11:01:02 -0700

    signals

--- djangoChallenge/settings.py
+++ djangoChallenge/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
+    'accounts.apps.AccountsConfig',
     'django_filters',
 ]
 
@@ -127,4 +127,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')  # upload profile 
+MEDIA_ROOT = os.path.join(BASE_DIR,'static/images')  # upload profile
